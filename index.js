33f7aa5a3d0380748c9ebd81f1ca70f966d1a580
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-15 23:27:00 +0600

    show single phone data using id

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 4000
 
 app.get('/', (req, res) => {
      res.send('My Phones data coming soon');
+});
+
+app.get('/phones', (req, res) => {
+     res.send(phones);
+});
+
+app.get('/phones/:id', (req, res) => {
+     const id = parseInt(req.params.id);
+     console.log('I need data for id: ', id);
+     const phone = phones.find(phone => phone.id === id) || {};
+     res.send(phone);
 });
 
 app.listen(port, () => {
