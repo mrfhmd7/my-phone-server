fe9298722d92362eac782d7a967fccf64e88ffb0
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-15 23:07:27 +0600

    add in phones

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000
 
 app.get('/', (req, res) => {
-     res.send('Phone data coming soon');
+     res.send('My Phones data coming soon');
 });
 
 app.listen(port, () => {
